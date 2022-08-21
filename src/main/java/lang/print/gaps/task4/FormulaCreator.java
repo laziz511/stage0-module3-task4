376f2713070b4a13ca70f46061cda60562690926
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-21 18:57:03 +0500

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,8 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        byte a = 6;
-        byte b = 5;
+        byte a = 15;
+        byte b = 20;
         int c = ((9*a*a - 5*b + 2 + a - 7) * ((a+b - 4*a*b)/2));
     }
 }
